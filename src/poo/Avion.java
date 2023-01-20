7d1fb22581d85a660cf318a89b181df4b7988d04
Christian Lisangola <jochri3@gmail.com>
2023-01-20 16:18:22 +0300

    21. Getters et setters

--- src/poo/Avion.java
+++ src/poo/Avion.java
@@ -1,7 +1,39 @@
 package poo;
 
 public class Avion {
-    String marque;
-    String modele;
-    int nombreSieges;
+    private String marque;
+    private String modele;
+    private int nombreSieges;
+
+
+    // Accesseurs
+    public String getMarque(){
+        return marque;
+    }
+
+    public String getModele(){
+        return modele;
+    }
+
+    public int getNombreSieges(){
+        return nombreSieges;
+    }
+    // Manipulateurs
+    public void setMarque(String marque){
+        if(marque.length()!=0){
+            this.marque=marque;
+        }
+    }
+
+    public void setModele(String modele){
+        if(modele.length()!=0){
+            this.modele=modele;
+        }
+    }
+
+    public void setNombreSieges(int nombreSieges){
+        if(nombreSieges>0){
+            this.nombreSieges=nombreSieges;
+        }
+    }
 }
--- src/poo/Main.java
+++ src/poo/Main.java
@@ -1,15 +1,25 @@
 package poo;
 
+import poo.vendredi_20_janvier.Tp1_Rectangle;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Avion airbus=new Avion();
-        airbus.marque="Airbus";
-        airbus.modele="A320";
-        airbus.nombreSieges=-150;
+        airbus.setMarque("Airbus");
+        airbus.setModele("A320");
+        airbus.setNombreSieges(150);
 
-        System.out.printf("Marque : %s\nModèle : %s\nNombre sièges : %d",airbus.marque,
-                airbus.modele,airbus.nombreSieges);
+        System.out.printf("Marque : %s\nModèle : %s\nNombre sièges : %d",airbus.getMarque(),
+                airbus.getModele(),airbus.getNombreSieges());
+
+//        var rectangle=new Tp1_Rectangle();
+//        rectangle.setLongueur(500);
+//        rectangle.setLargeur(250);
+//        System.out.printf("Aire : %.2f\nPerimetre : %.2f",rectangle.calculAire(),
+//                rectangle.calculPerimetre());
+
+
     }
 }
