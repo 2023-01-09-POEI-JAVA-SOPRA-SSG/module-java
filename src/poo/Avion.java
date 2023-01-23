1997c3f5b6f7e1a0e4a25212025bfcf51fabc012
Christian Lisangola <jochri3@gmail.com>
2023-01-23 12:14:03 +0300

    22. Gestion des erreurs

--- src/poo/Avion.java
+++ src/poo/Avion.java
@@ -5,6 +5,27 @@ public class Avion {
     private String modele;
     private int nombreSieges;
 
+    public Avion(String marque,String modele,int nombreSieges){
+//        Approche sans setters
+//       if(marque.length()!=0){
+//           this.marque=marque;
+//       }
+//
+//       if(modele.length()!=0){
+//           this.modele=modele;
+//       }
+//
+//
+//        if(nombreSieges>0){
+//            this.nombreSieges=nombreSieges;
+//        }
+//
+
+//        Utilisation des setters
+        this.setMarque(marque);
+        this.setModele(modele);
+        this.setNombreSieges(nombreSieges);
+    }
 
     // Accesseurs
     public String getMarque(){
